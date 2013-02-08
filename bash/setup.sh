ba2a8d920d209b1ccf2d1acee714e291c0753557
Vicente Alencar <vicente@highlevel-it.net>
2013-02-07 20:33:41 -0800

    Updating screenrc and setup.sh

--- bash/setup.sh
+++ bash/setup.sh
@@ -6,3 +6,4 @@ ln -nfs $CURRENT_DIR/../vim/.vimrc ~/.vimrc
 ln -nfs $CURRENT_DIR/../git/.gitconfig ~/.gitconfig
 ln -nfs $CURRENT_DIR/.bashrc ~/.bashrc
 ln -nfs $CURRENT_DIR/.bash_profile ~/.bash_profile
+ln -nfs $CURRENT_DIR/.screenrc ~/.screenrc
